4421a7972dfccaf11e321d27fe4ba538fe59f0b0
DaniloLulaj96 <danilo.lulaj@gmail.com>
2024-03-13 01:56:17 +0100

    print members in console

--- script.js
+++ script.js
@@ -10,3 +10,25 @@ BONUS 1:
 Trasformare la stringa foto in una immagine effettiva
 BONUS 2:
 Organizzare i singoli membri in card/schede */
+
+// creo un oggetto array che contiene tutti i membri del team e li inserisco all'interno di una variabile
+const team = 
+[
+    { name: 'Jonh', role: 'CEO', pic: './img/pic1.jfif',},
+    { name: 'Joseph', role: 'CFO', pic: './img/pic2.jfif',},
+    { name: 'Jane', role: 'CMO', pic: './img/pic3.jfif',},
+    { name: 'Jessica', role: 'CSO', pic: './img/pic4.jfif',},
+    { name: 'Philip', role: 'CTO', pic: './img/pic1.jfif',}
+]
+
+// seleziono la griglia 
+document.querySelector('row')
+
+// creo un ciclo for per stampare tutti i membri in console
+for (let i = 0; i < team.length; i++) {
+    const member = team[i];
+
+    console.log( member.name, member.role, member.pic,)
+}
+
+
